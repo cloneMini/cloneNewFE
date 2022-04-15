d00815c1f00f8daf25979a7a962c70153c1bbc24
hoontail <sper456@naver.com>
2022-04-15 20:38:34 +0900

    어디에요 여기에요?

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import ListPage from '../pages/ListPage';
 import {Route} from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ConnectedRouter } from "connected-react-router";
-import { history } from "../redux/configureStore";
+
+
 
 function App() {
   return (
