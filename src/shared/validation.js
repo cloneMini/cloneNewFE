f0ec07afbb63966c848cffbdaa19f227cb65b906
yooDeve <janhold@naver.com>
2022-04-18 20:16:49 +0900

    회원가입 완료

--- src/shared/validation.js
+++ src/shared/validation.js
@@ -1,18 +1,22 @@
 export const idVal = (nickname) => {
-    const _reg = /[A-Za-z0-9]{4,20}$/;
+  const _reg = /[A-Za-z0-9]{4,20}$/;
+  console.log(_reg.test(nickname));
+  return _reg.test(nickname);
+};
+
+export const idVal1 = (nickname) => {
   
-    return _reg.test(nickname);
-  };
-  
-  export const pwdVal = (password) => {
-    const _reg = /[A-Za-z0-9]{4,30}$/;
-  
-    return _reg.test(password);
-  };
-  
-  export const searchKeywordValidation = (keyword) => {
-    const _reg = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣\s|0-9a-zA-z]{1,30}$/;
-  
-    return _reg.test(keyword);
-  };
-  
+  const _reg = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  return _reg.test(nickname);
+};
+export const pwdVal = (password) => {
+  const _reg = /[A-Za-z0-9]{4,30}$/;
+
+  return _reg.test(password);
+};
+
+export const searchKeywordValidation = (keyword) => {
+  const _reg = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣\s|0-9a-zA-z]{1,30}$/;
+
+  return _reg.test(keyword);
+};
