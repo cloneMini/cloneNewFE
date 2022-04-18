65d3a6a2d031756487acd8d9421786804f79ddbc
hoontail <sper456@naver.com>
2022-04-18 09:53:28 +0900

    온클릭 스크롤 이동 구현

--- src/detailcompo/CommentBox.js
+++ src/detailcompo/CommentBox.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
+
 const CommentBox = (props) => {
-  // const comment = useSelector((state) => state.comment.list.comment)
+
 
 
 
